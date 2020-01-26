fdfe415e41091847a58ea4e1f79ba8547741b467
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-01-25 22:42:20 -0600

    Mostar el listado de los artículos con su imagen desde la api

--- blogreact/src/components/Articles.js
+++ blogreact/src/components/Articles.js
@@ -1,15 +1,85 @@
-import React,{Component} from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import Global from '../Global';
+import imagen from "../assets/images/default.png";
+class Articles extends Component {
 
-class Articles extends Component{
+   url = Global.url;
+    
+  state = {
+    articles: [],
+    status: null
+  }
 
-    render(){
+  componentDidMount() {
+    {
+      /*Antes de imprimir la vista cargame el metodo*/
+    }
+    this.getArticles();
+  }
+  getArticles = () => {
+    axios.get(this.url+"articles").then(res => {
+      this.setState({
+        articles: res.data.articles,
+        status: "success"
+      });
+      console.log(this.State);
+    });
+    console.log("get articles");
+  };
+  render() {
+
+    if (this.state.articles.length >= 1) {
+
+     var listArticles = this.state.articles.map((article)=>{
         return(
-          <div id="articles">
-          <h1>Articulos</h1>
-          </div>
+            <article className="article-item" id="article-template">
+            <div class="image-wrap">
+
+            {article.image !== null ?(
+
+                <img src={this.url+'get-image/'+ article.image} alt={article.title} />
+
+
+            ):(
+                <img src={imagen} alt={article.title} />
+            )
+            
+
+            }
+             </div>
+             <h2>{article.title}</h2>
+                <span className="date">
+                {article.date}
+                </span>
+             <a href="#">Leer más</a>
+
+             <div className="clearfix"></div>
+        </article>
+        );
+     });
+
+      return (
+        <div id="articles">
+          <h1>{listArticles}</h1>
+        </div>
+      );
+    } else if (this.state.articles.length === 0 && this.state.status === "success") {
+      return (
+        <div id="articles">
+          <h2 className="subheader">No hay artículos para mostrar</h2>
+          <p>Todavía no hay contenido en está sección</p>
+        </div>
+      );
+    }else{
+        return(
+      <div id="articles">
+        <h2 className="subheader">Cargando....</h2>
+        <p>Espere mientras carga el contenido</p>
+      </div>
         );
     }
+  }
 }
 
 export default Articles;
