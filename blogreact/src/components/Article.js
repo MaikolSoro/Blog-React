87d0c42280a09aa6ab739d0fd4ef78ad3a3c6bfe
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-01-26 18:52:47 -0600

    mostramos el contenido del articulo con su imagen respectiva y además agregamos los botones de eliminar y editar un articulo

--- blogreact/src/components/Article.js
+++ blogreact/src/components/Article.js
@@ -1,43 +1,93 @@
 import React, { Component } from "react";
-import { Redirect,Link} from 'react-router-dom';
-import axios from 'axios';
-import Global from '../Global';
+import { Redirect, Link } from "react-router-dom";
+import axios from "axios";
+import Global from "../Global";
 import Sidebar from "./Sidebar";
-class Article extends Component{
+import imagen from "../assets/images/default.png";
 
-    render(){
-        return(
-            <div className="center">
-            <section id="content">
+import Moment from 'react-moment';
+import 'moment/locale/es';
 
-                    <article className="article-item article-detail">
-                        <div className="image-wrap">
-                            <img src="https://unhabitatmejor.leroymerlin.es/sites/default/files/styles/header_category/public/2018-10/4%20paisaje%20macedonia.jpg?itok=AELknmF8" alt="Paisaje" />
-                        </div>
-    
-                        <h1 className="subheader">Articulo de prueba</h1>
-                        <span className="date">
-                            Hace 5 minutos
-                        </span>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet consectetur dui. Vestibulum ac convallis urna, vitae porta massa. Mauris sit amet nisi in metus tempor convallis. Nulla nec euismod turpis. Cras luctus lorem et nisl dapibus aliquet. Curabitur lorem nunc, tristique a felis ac, vehicula laoreet ante. Ut auctor orci turpis. Cras sit amet placerat nulla, feugiat eleifend metus. Mauris nec convallis lectus. In gravida sapien in iaculis vulputate. Aliquam a rhoncus elit, sit amet pretium nisl. Vivamus egestas facilisis viverra. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vel lorem est. Cras et sapien finibus, mattis est in, condimentum nisl.
-                        </p>
-                        <p>
-                            Aenean vel orci a tellus porttitor eleifend. Integer tincidunt porta fermentum. Aenean vitae enim iaculis, sollicitudin risus in, interdum justo. Donec semper metus ac nibh maximus venenatis. Cras sodales nisl metus, a placerat risus tristique ornare. Ut finibus nisi a ante tincidunt hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut eget venenatis augue, id molestie eros. Quisque leo risus, pellentesque mattis libero id, pharetra tempor turpis.
-                        </p>
-                        <p>  
-                            Donec fermentum ligula at ultrices faucibus. Proin facilisis est vitae vehicula finibus. Praesent rutrum eleifend ligula, sit amet molestie dolor interdum nec. Cras sodales odio nec diam posuere, mollis aliquam magna tempus. Fusce lobortis maximus dapibus. Curabitur aliquam vehicula ultricies. Nulla sed vulputate erat. 
-                        </p>
+class Article extends Component {
+  url = Global.url;
+  state = {
+    article: false,
+    status: null
+  };
 
-                        <div className="clearfix"></div>
-                    </article>
-                
-            </section>
+  componentWillMount() {
+    this.getArticle();
+  }
+  getArticle = () => {
+    var id = this.props.match.params.id;
 
-            <Sidebar/>
+    axios.get(this.url + "article/" + id).then(res => {
+      this.setState({
+        article: res.data.article,
+        status: "succes"
+      });
+    }).catch( err =>{
+        this.setState({
+            article: [],
+            status: "succes"
+          });
+    });
+  };
+  render() {
+    var article = this.state.article;
+    return (
+      <div className="center">
+        <section id="content">
+          {article &&
+            <article className="article-item article-detail">
+              <div className="image-wrap">
+              {article.image !== null ?(
 
+                <img src={this.url+'get-image/'+ article.image} alt={article.title} />
+
+
+            ):(
+                <img src={imagen} alt={article.title} />
+            )
+            
+
+            }
+              </div>
+
+              <h1 className="subheader">
+                {article.title}
+              </h1>
+              <span className="date">
+              <Moment locale="es" fromNow>{article.date}</Moment>
+              </span>
+              <p>
+               {article.content}
+              </p>
+              <a href ="#" className="btn btn-danger">Eliminar</a>
+              <a href ="#" className="btn btn-warning">Editar</a>
+
+
+              <div className="clearfix" />
+            </article>
+        }
+        {!article && this.state.status == 'success' &&
+            <div id="article">
+            <h2 className="subheader">El artículo no exíste</h2>
+            <p>Intentalo de nuevo más tarde</p>
             </div>
-        );
-    }
+
+        }
+        {this.state.status == null &&
+            <div id="article">
+            <h2 className="subheader">Cargando...</h2>
+            <p>Espere unos segundos</p>
+            </div>
+        }
+        </section>
+
+        <Sidebar />
+      </div>
+    );
+  }
 }
 export default Article;
